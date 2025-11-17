d80f358e2aea1c7c294c8746bcb7e3fad305e1e9
vochidat0 <dat.usth2000@gmail.com>
2025-11-17 11:23:40 +1100

    fix(lsp): update new version and reconfigure

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 require("vochidat0.core")
 require("vochidat0.lazy")
+require("vochidat0.lsp")
--- .config/nvim/lua/vochidat0/plugins/colorscheme.lua
+++ .config/nvim/lua/vochidat0/plugins/colorscheme.lua
@@ -9,13 +9,10 @@
 -- }
 
 return {
-  "sainnhe/gruvbox-material",
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.cmd.colorscheme("gruvbox-material")
+    vim.cmd.colorscheme("gruvbox")
   end,
 }
